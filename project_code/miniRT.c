3108ee0862259254990d2be995095bd7985ca520
Ahmed Yassin <ayassin@lab1r4s9.42abudhabi.ae>
2022-12-17 18:13:57 +0400

    basic main

--- project_code/miniRT.c
+++ project_code/miniRT.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniRT.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahsalem <ahsalem@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 21:08:55 by ayassin           #+#    #+#             */
-/*   Updated: 2022/12/17 16:07:04 by ahsalem          ###   ########.fr       */
+/*   Updated: 2022/12/17 18:13:09 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 int	main(int argc, char **argv)
 {
+	t_win	win;
+	t_img	img;
+
+	// read map
 	if (not_valid_file(argc, argv))
 		return (1);
+	window_setup(&win, &img, "miniRT");
+	// put prossing comand inside redraw;
+	redraw(&win, win.img, win.img->x_pos, win.img->y_pos);
+	mlx_hook(win.win_ptr, 2, 1L << 0, key_hook, &win);
+	mlx_hook(win.win_ptr, 17, 0, exit_code, &win);
+	mlx_loop(win.mlx_ptr);
 	return (0);
 }
