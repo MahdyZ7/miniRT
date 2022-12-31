08fef767b0d8772a3563682f853d7f52538a4a04
Ahmed Yassin <ayassin@lab1r4s9.42abudhabi.ae>
2022-12-31 19:51:01 +0400

    home files

--- project_code/basic_raytracing.c
+++ project_code/basic_raytracing.c
@@ -6,7 +6,7 @@
 /*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 18:55:05 by ayassin           #+#    #+#             */
-/*   Updated: 2022/12/31 14:51:36 by ayassin          ###   ########.fr       */
+/*   Updated: 2022/12/31 19:29:52 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,26 +56,18 @@ int		compute_color(t_vec *origin, t_vec *dir, t_sphere *sphere, t_scene *scene, 
 float	hit_sphere(t_sphere *sphere, t_vec *origin, t_vec *dir, float t_min)
 {
 	t_vec	oc;
-	float	a;
-	float	b;
-	float	c;
-	float	discriminant;
-	float	root[2];
+	float	quad[3];
 
 	vec_init(&oc, origin->x - sphere->center.x,  origin->y - sphere->center.y,  origin->z - sphere->center.z);
-	a = vec_dot(dir, dir);
-	b = 2.0 * vec_dot(&oc, dir);
-	c = vec_dot(&oc, &oc) - pow(sphere->diameter / 2, 2);
-	discriminant = b * b - 4 * a * c;
-	if (discriminant < 0)
-		return (INFINITY);
-	root[0] = (-b - sqrt(discriminant)) / (2.0 * a);
-	root[1] = (-b + sqrt(discriminant)) / (2.0 * a);
-	if (!(t_min < root[0] && t_min < root[1]))
+	quad[0] = vec_dot(dir, dir);
+	quad[1] = 2.0 * vec_dot(&oc, dir);
+	quad[2] = vec_dot(&oc, &oc) - pow(sphere->diameter / 2, 2);
+	oc = qudratic_equation(quad[0], quad[1], quad[2]);
+	if (!(t_min < oc.x && t_min < oc.y ))
 			return (INFINITY);
-	if (root[1] < t_min || root[0] < root[1])
-		return (root[0]);
-	return (root[1]);
+	if (oc.y < t_min || oc.x < oc.y)
+		return (oc.x);
+	return (oc.y);
 }
 
 int	trace_ray(t_vec *dir, float t_min, t_scene *scene)
@@ -87,7 +79,7 @@ int	trace_ray(t_vec *dir, float t_min, t_scene *scene)
 
 	closest_t = INFINITY;
 	closest_sphere = NULL;
-	for (int i = 0; i < scene->spheres->n_spheres; i++)
+	for (int i = 0; i < scene->n_spheres; i++)
 	{
 		temp_t = hit_sphere(&(scene->spheres[i]), &scene->camera.view_point, dir, t_min);
 		if (temp_t < closest_t)
@@ -119,11 +111,12 @@ void	basic_raytracing(t_img *img)
 		{
 			r.new_x = (2 * ((r.x + 0.5) * r.invWidth) - 1) * r.angle * r.aspectratio; 
 			r.new_y = (1 - 2 * ((r.y + 0.5) * r.invHeight)) * r.angle;
-			vec_init(&r.dir, r.new_x,r. new_y, 1);
-			// r.color = trace_ray(&r.dir, 1, img->scene);
-			r.color = trace_plane(&r.dir, 1, img->scene);
+			vec_init(&r.dir, r.new_x,r. new_y, img->scene->camera.view_point.z + 1);
+			r.color = trace_ray(&r.dir, 1, img->scene);
+			// r.color = trace_plane(&r.dir, 1, img->scene);
 			pixel_put(img->scene->win->img, r.x, r.y, r.color);
 			++r.y;
+			
 		}
 		r.x++;
 	}
