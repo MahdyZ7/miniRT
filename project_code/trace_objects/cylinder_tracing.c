c81a1bf2a6487d2fbd439262a3a4b3d50c4e740e
Ahmed Yassin <ayassin@lab2r2s10.42abudhabi.ae>
2023-01-02 17:37:48 +0400

    cylider in z direction works

--- project_code/trace_objects/cylinder_tracing.c
+++ project_code/trace_objects/cylinder_tracing.c
@@ -4,24 +4,31 @@
 
 float	hit_cylinder(t_cylinder *cylinder, t_vec *origin, t_vec *dir, float t_min)
  {
+	t_vec	oc;
+
+	oc = vec_sub(origin, &(cylinder->pos));
     float	a = (dir->x * dir->x) + (dir->y * dir->y);
-    float	b = 2 * ((dir->x * (origin->x -  cylinder->pos.x)) + (dir->y * (origin->y - cylinder->pos.y)));
-    float	c = ((origin->x - cylinder->pos.x) * (origin->x - cylinder->pos.x))
-	+ ((origin->y - cylinder->pos.y) 
-	* (origin->y - cylinder->pos.y)) - ((cylinder->diameter / 2) * (cylinder->diameter /2));
+    float	b = 2 * ((dir->x * oc.x) + (dir->y * oc.y));
+    float	c = (oc.x * oc.x)+ (oc.y * oc.y)
+		- ((cylinder->diameter / 2) * (cylinder->diameter /2));
     float	discriminant = (b * b) - (4 * a * c);
     
-    if (discriminant < 0.0000001)
+    if (discriminant < 0)
         return (INFINITY);
     float	t1 = (-b - sqrt(discriminant)) / (2 * a);
     float	t2 = (-b + sqrt(discriminant)) / (2 * a);
 	if (!(t_min < t1 && t_min < t2))
 	{
+		// return (INFINITY);
 		return (check_capped_part(t1, t2, origin, dir, cylinder));
 	}
-	if (t2 < t_min || t1 < t2)
+	float z1 = origin->z + t1 * dir->z;
+	float z2 = origin->z + t2 * dir->z;
+	if ((t2 < t_min || t1 < t2) && z1 > cylinder->pos.z && z1 < cylinder->pos.z + cylinder->height)
 		return (t1);
-	return (t2);
+	if (z2 > cylinder->pos.z && z2 < cylinder->pos.z + cylinder->height)
+		return (t2);
+	return (INFINITY);
 
 }
 
