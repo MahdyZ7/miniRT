b61fbb6ed07c618b278ffebc4fb0802ca12b64f7
Ahmed Salem <ahsalem@lab1r4s11.42abudhabi.ae>
2023-01-07 22:38:25 +0400

    working updates with norm

--- project_code/ft_math/vectors_utils.c
+++ project_code/ft_math/vectors_utils.c
@@ -6,7 +6,7 @@
 /*   By: ahsalem <ahsalem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:16:26 by ahsalem           #+#    #+#             */
-/*   Updated: 2023/01/07 18:16:46 by ahsalem          ###   ########.fr       */
+/*   Updated: 2023/01/07 22:35:14 by ahsalem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,14 @@ t_vec	vec_multiply_two_vectors(t_vec *vec1, t_vec *vec2)
 	result.y = vec1->y * vec2->y;
 	result.z = vec1->z * vec2->z;
 	return (result);
+}
+
+void	normalize(t_vec *v)
+{
+	float	inv;
+
+	inv = 1 / sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+	v->x *= inv;
+	v->y *= inv;
+	v->z *= inv;
 }
